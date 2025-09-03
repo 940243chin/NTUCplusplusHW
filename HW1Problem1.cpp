f6deefe383798d559d9ea37661f255a8af68a942
Robert <940243chin@gmail.com>
2025-09-03 18:42:51 +0800

    first commit

---
+++ HW1Problem1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+Problem 1
+int main() {
+    int det=0,age=0;
+    cout << "Input:\n";
+    cin >> det >> age;
+    cout << "\n\n\nOutput:\n";
+    if(det == 1 && age>=18)
+        cout << "Yes\n";
+    if(det == 0 && age>=16)
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+return 0;
+}
